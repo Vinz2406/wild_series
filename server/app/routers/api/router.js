37494b87526d06c2ee7e486f44434498a71621de
wilder <vincent.vanvolsem@gmail.com>
2024-05-24 17:28:47 +0200

    harmonia routing

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
+
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
@@ -10,6 +15,9 @@ const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
 
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 /* ************************************************************************* */
 
 module.exports = router;
