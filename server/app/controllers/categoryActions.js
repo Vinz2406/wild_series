3147f74637f6955d0715c02933589c1be9402fda
VINCENT VAN VOLSEM <vincent.vanvolsem@gmail.com>
2024-05-28 22:57:06 +0200

    Harmonia : Créer ton premier Repository

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,4 +1,5 @@
 // Some data to make the trick
+const tables = require("../../database/tables");
 
 const categories = [
     {
@@ -13,17 +14,11 @@ const categories = [
   
   // Declare the actions
 
-  const browse = (req, res) => {
-    if (req.query.q != null) {
-      const filteredCategories = categories.filter((categorie) =>
-        categorie.synopsis.includes(req.query.q)
-      );
-  
-      res.json(filteredCategories);
-    } else {
-      res.json(categories);
-    }
-  };
+  const browse = async (req, res) => {
+      const categoriesFromDB = await tables.category.readAll();
+    
+      res.json(categoriesFromDB);
+    };
 
   const read = (req, res) => {
     const parsedId = parseInt(req.params.id, 10);
